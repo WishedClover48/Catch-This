63d1692f3ba8616dd0920c685592f4cd57a1bdad
Demian-7 <demianlozanoriedel@gmail.com>
2025-09-18 20:42:35 -0300

    Modify player and camera

--- Assets/BulletMovement.cs
+++ Assets/BulletMovement.cs
@@ -8,17 +8,20 @@ public class BulletMovement : MonoBehaviourPun
 {
     [SerializeField] private string source;
     [SerializeField] private float speed;
+    [SerializeField] private LayerMask Mask;
     void Update()
     {
-        transform.position += Vector3.forward * (speed * Time.deltaTime);
+        transform.position += transform.forward * (speed * Time.deltaTime);
     }
     private void OnTriggerEnter(Collider collision)
     {
-        if(collision.CompareTag("Player"))
+        if (LayerMask.Equals(Mask, collision.gameObject.layer))
         {
-            //collision.gameObject.SetActive(false);
-            PlayerMovement playerHitted = collision.GetComponent<PlayerMovement>();
-            playerHitted.Pv.RPC("KillPlayer",playerHitted.Pv.Owner);
+            if (collision.gameObject.layer == LayerMask.GetMask("Player"))
+            {
+                PlayerMovement playerHitted = collision.GetComponent<PlayerMovement>();
+                playerHitted.Pv.RPC("KillPlayer", playerHitted.Pv.Owner);
+            }            
             PhotonNetwork.Destroy(gameObject);
         }
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,10 @@ using UnityEngine.UIElements;
 public class PlayerMovement : MonoBehaviourPunCallbacks
 {
     public float moveSpeed = 5f;
-    private Camera playerCam;
+    [SerializeField]private Camera playerCam;
+    private Vector3 camaraStartingPosition;
     [SerializeField] private GameObject bulletPrefab;
+    public LayerMask groundMask;
 
     public Action<int, string> OnHit;
     private PhotonView pv;
@@ -18,13 +20,12 @@ public class PlayerMovement : MonoBehaviourPunCallbacks
     void Start()
     {
         pv = GetComponent<PhotonView>();
-        // Get the camera from the child
-        playerCam = GetComponentInChildren<Camera>();
 
         // Enable it only for the local player
         if (photonView.IsMine)
         {
             if (playerCam != null) playerCam.enabled = true;
+            camaraStartingPosition=playerCam.transform.position;
             //photonView.RPC("AddPlayer", RpcTarget.AllBuffered, PhotonNetwork.LocalPlayer, this.gameObject); //Modificar
         }
 
@@ -35,19 +36,34 @@ public class PlayerMovement : MonoBehaviourPunCallbacks
     {
         // Ensure only the local player moves this instance
         if (!photonView.IsMine) return;
-
+        LookAt();   
         // Get input
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveZ = Input.GetAxisRaw("Vertical");
 
         // Calculate direction
         Vector3 movement = new Vector3(moveX, 0f, moveZ).normalized;
+        playerCam.transform.position = camaraStartingPosition+transform.position;
 
         // Move the player
         transform.position += movement * (moveSpeed * Time.deltaTime);
 
         if (Input.GetMouseButtonDown(0)) {
+            
             PhotonNetwork.Instantiate(bulletPrefab.name, transform.position + Vector3.forward * 2, transform.rotation);
+        }
+    }
+    void LookAt()
+    {
+        Ray ray = playerCam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // Raycast to the ground
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundMask))
+        {
+            Vector3 lookAtPoint = hit.point;
+            lookAtPoint.y = transform.position.y; // Keep only horizontal rotation
+            transform.LookAt(lookAtPoint);
         }
     }
 
