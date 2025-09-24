c0793ed368d1c80bff36978aac1a0a0a222b29bc
IgnacioPuk <49323352+pux99@users.noreply.github.com>
2025-09-24 20:36:17 -0300

    gameplay twicks and fix bullets not colition with obstacles

--- Assets/Scripts/BulletMovement.cs
+++ Assets/Scripts/BulletMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,7 +14,15 @@ public class BulletMovement : MonoBehaviourPun
     [SerializeField] private LayerMask Mask;
     [SerializeField] private LayerMask PlayerMask;
     [SerializeField] private GameObject owner;
+    [SerializeField] private float lifeTime;
     [SerializeField] private Player ownerID;
+
+    private void Start()
+    {
+        if (photonView.IsMine)
+            StartCoroutine(Life());
+    }
+
     void Update()
     {
         transform.position += transform.forward * (speed * Time.deltaTime);
@@ -21,25 +30,27 @@ public class BulletMovement : MonoBehaviourPun
     private void OnTriggerEnter(Collider collision)
     {
         if(!photonView.IsMine) return;
-        Debug.Log("pre pre pre kill");
         if (InMask(Mask, collision.gameObject.layer)&&collision.gameObject!=owner)
         {
-            Debug.Log("pre pre kill");
             if (InMask(PlayerMask, collision.gameObject.layer))
             {
                 //PlayerMovement playerHitted = collision.GetComponent<PlayerMovement>();
                 //playerHitted.Pv.RPC("KillPlayer", playerHitted.Pv.Owner);
-                Debug.Log("pre kill");
                 PhotonView pv = collision.GetComponent<PhotonView>();
                 if (pv != null)
                 {
                     pv.RPC("KillPlayer", pv.Owner);
                     PhotonNetwork.LocalPlayer.AddScore(1);
-                    Debug.Log("kill");
                 }
             }            
             PhotonNetwork.Destroy(gameObject);
         }
+    }
+
+    IEnumerator Life()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        PhotonNetwork.Destroy(gameObject);
     }
 
     public void SetUpOwner(GameObject ownerGameObject, Player player)
