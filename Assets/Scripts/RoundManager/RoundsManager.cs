29a98d90c539968c1e45016a619900ef91e9c7e2
IgnacioPuk <49323352+pux99@users.noreply.github.com>
2025-09-24 14:21:43 -0300

    round change

--- Assets/Scripts/RoundManager/RoundsManager.cs
+++ Assets/Scripts/RoundManager/RoundsManager.cs
@@ -5,6 +5,8 @@ using System.Diagnostics;
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun.UtilityScripts;
 
 public class RoundsManager : MonoBehaviourPunCallbacks
 {
@@ -12,6 +14,7 @@ public class RoundsManager : MonoBehaviourPunCallbacks
     private float _roundTimer = 0f;
     private bool _roundActive = false;
     public float RoundDuration => _roundDuration;
+    
     void Start()
     {
         if (PhotonNetwork.IsMasterClient)
@@ -43,14 +46,36 @@ public class RoundsManager : MonoBehaviourPunCallbacks
     {
         _roundActive = false;
         UnityEngine.Debug.Log("Round finished");
+        RecalculateAlivePlayers();
         // Notify everyone that the round ended
         photonView.RPC("OnRoundEnd", RpcTarget.All);
+    }
+    private void RecalculateAlivePlayers()
+    {
+        List<Player> alivePlayers  = new List<Player>();
+        int alive = 0;
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            if (p.CustomProperties.TryGetValue("IsDead", out object isDeadObj))
+                if (!(bool)isDeadObj)
+                    alivePlayers .Add(p);
+                else
+                    alive++;
+        }
+        int totalPlayers = PhotonNetwork.CountOfPlayersInRooms;
+        int pointsToDistribute = totalPlayers * 2 - (totalPlayers - alivePlayers.Count);
+
+        foreach (Player player in alivePlayers)
+        {
+            int points = pointsToDistribute / alivePlayers.Count;
+            player.AddScore(points);
+        }
     }
 
     [PunRPC]
     void OnRoundEnd()
     {
-        PhotonNetwork.LoadLevel("Lobby");
+        // PhotonNetwork.LoadLevel("Lobby");
         // Add local reaction to round end
     }
 }
