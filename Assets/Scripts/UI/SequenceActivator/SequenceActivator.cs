a35742c3ce847fd9aadb6b8ee5c66e0966a5ef3f
IgnacioPuk <49323352+pux99@users.noreply.github.com>
2025-09-24 23:20:18 -0300

    Change ready set go

--- Assets/Scripts/UI/SequenceActivator/SequenceActivator.cs
+++ Assets/Scripts/UI/SequenceActivator/SequenceActivator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SequenceActivator : MonoBehaviour
 {
@@ -31,15 +32,34 @@ public class SequenceActivator : MonoBehaviour
             currentPair.gObject.SetActive(false);
             pair.gObject.SetActive(true);
             currentPair=pair;
+            StartCoroutine(Expand(pair));
             yield return new WaitForSeconds(pair.time);
         }
         currentPair.gObject.SetActive(false);
         SequenceFinished?.Invoke();
+    }
+
+    private IEnumerator Expand(ObjectTimePair pair)
+    {
+        var time=0f;
+        var initialScale = pair.gObject.transform.localScale;
+        var targetScale = initialScale*pair.Expacion;
+        var image = pair.gObject.GetComponent<Image>();
+        while (time < pair.time)
+        {
+            time += Time.deltaTime;
+            float t = Mathf.Clamp01(time / pair.time);
+            pair.gObject.transform.localScale = Vector3.Lerp(initialScale, targetScale, t);
+            if(t >= 0.7f)
+                image.color = Color.Lerp(pair.gObject.GetComponent<Image>().color, new Color(1,1,1,0.3f), t-0.7f);
+            yield return 0;
+        }
     }
     [Serializable]
     public struct ObjectTimePair
     {
         public GameObject gObject;
         public float time;
+        public float Expacion;
     }
 }
