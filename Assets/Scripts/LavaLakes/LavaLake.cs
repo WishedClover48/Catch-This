15afbe21f6b3fc59325306553e373c16e27e53fe
EgiglioUade <146669271+Zeki19@users.noreply.github.com>
2025-11-20 14:19:17 -0300

    Update LavaLake.cs

--- Assets/Scripts/LavaLakes/LavaLake.cs
+++ Assets/Scripts/LavaLakes/LavaLake.cs
@@ -67,7 +67,8 @@ public class LavaLake : MonoBehaviourPunCallbacks
     }
     private void OnTriggerEnter(Collider other)
     {
-        other.gameObject.GetComponent<Notif>()?.ShowText("Danger Lava");
+        //other.gameObject.GetComponent<Notif>()?.ShowText("Danger Lava");
+        
         if (!PhotonNetwork.IsMasterClient) return;
 
         if (!_playersOnLava.ContainsKey(other.gameObject))
@@ -77,7 +78,7 @@ public class LavaLake : MonoBehaviourPunCallbacks
     }
     private void OnTriggerExit(Collider other)
     {
-        other.gameObject.GetComponent<Notif>().HideText();
+        //other.gameObject.GetComponent<Notif>()?.HideText();
         
         if (!PhotonNetwork.IsMasterClient) return;
 
